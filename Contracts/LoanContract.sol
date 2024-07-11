91293d93d143a75e48535519533a42d99ebc9ac9
alan <alanhungyl@gmail.com>
2024-07-11 17:14:12 +0800

    commit

--- Contracts/LoanContract.sol
+++ Contracts/LoanContract.sol
@@ -5,13 +5,13 @@ import "./CollateralCoin.sol";
 contract LoanContract {
     address private buyer;
     address private seller;
-    address private contractOwner;
+    address private contractOwner;      // The only of the factory contract, possibiliy CMU
 
-    uint256 private collateralAmount;
-    uint256 private loanAmount;
+    uint256 private collateralAmount;   // The amount of collateral coins deposited
+    uint256 private loanAmount;         // The amount of loan coins
     
-    uint256 public createTime;
-    uint256 public loanDuration;
+    uint256 public createTime;          // The creation time of this contract
+    uint256 public deadline;            // Buyers should repay the loan before this time
 
     // Constructor: fill in the variables passed by the ContractFactory
     constructor(address _buyer, address _seller, address _contractOwner, uint256 _collateralAmount, uint256 _loanAmount, uint256 _loanDuration) {
@@ -21,7 +21,7 @@ contract LoanContract {
         collateralAmount = _collateralAmount;
         loanAmount = _loanAmount;
         createTime = block.timestamp;
-        loanDuration = _loanDuration;
+        deadline = createTime + _loanDuration * 1 days;     // Calculate the deadline by adding the duration(in days) to the creation time
     }
 
     // Modifier: check if the msg.sender is the buyer, seller or the owner of the factory
@@ -30,6 +30,19 @@ contract LoanContract {
         _;
     }
 
+    // Get the collateral amount
+    function getCollateralAmount() public view onlyOwner() returns (uint256) {
+        return collateralAmount;
+    }
 
+    // Get the loan amount
+    function getLoanAmount() public view onlyOwner() returns (uint256) {
+        return loanAmount;
+    }
+
+    // Get the deadline
+    function getDeadline() public view onlyOwner() returns (uint256) {
+        return deadline;
+    }
 
 }
