81207efa090c4bf4e5d6c07c2023d941c84c1b17
alan <alanhungyl@gmail.com>
2024-07-15 17:17:52 +0800

    added indexToAddress & addressToIndex function

--- Contracts/LoanContractFactory.sol
+++ Contracts/LoanContractFactory.sol
@@ -6,7 +6,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract LoanContractFactory is Ownable {
     LoanContract[] private Contracts;
     uint256 private totalContracts;
+
     mapping(uint256 => address) private indexToContractAddress;
+    mapping(address => uint256) private contractAddressToIndex;
 
     // Set the owner of the factory contract = msg.sender and do some initalizations
     constructor() Ownable(msg.sender) {
@@ -17,9 +19,9 @@ contract LoanContractFactory is Ownable {
     function createLoanContract(
         address buyer,
         address seller,
-        uint colleteralAmount,
-        uint loanAmount,
-        uint loanDuration,
+        uint256 colleteralAmount,
+        uint256 loanAmount,
+        uint256 loanDuration,
         address collateralCoinAddress,
         address loanCoinAddress
     ) public onlyOwner {
@@ -36,15 +38,21 @@ contract LoanContractFactory is Ownable {
         );
         Contracts.push(newContract);
         indexToContractAddress[totalContracts] = address(newContract);
+        contractAddressToIndex[address(newContract)] = totalContracts;
         totalContracts++;
     }
 
-    // Get the whole contract and return to the backend/ frontend, maybe in .json format
-    // Can only be called by the factory owner
-    function getContract(
-        uint index
-    ) public view onlyOwner returns (LoanContract) {
+    // Contract only can get the whole contract and return to the backend/ frontend, maybe in .json format
+    function getContract(uint256 index) public view onlyOwner returns (LoanContract) {
         require(index < totalContracts, "Index out of range");
         return Contracts[index];
+    }
+
+    function getAddressWithIndex(uint256 index) public view onlyOwner returns (address) {
+        return indexToContractAddress[index];
+    }
+
+    function getIndexWithAddress(address contractAddress) public view onlyOwner returns (uint256) {
+        return contractAddressToIndex[contractAddress];
     }
 }
