3ebcd64bf8473a2192335930958a3d73dc0bda19
alan <alanhungyl@gmail.com>
2024-07-16 11:44:28 +0800

    removed duplicate dependency

--- Contracts/LoanContractFactory.sol
+++ Contracts/LoanContractFactory.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: None
 pragma solidity ^0.8.0;
 import "./LoanContract.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract LoanContractFactory is Ownable {
     LoanContract[] private Contracts;
