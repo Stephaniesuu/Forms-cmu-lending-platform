e7254733c13c1f70ab3196773278533a48e47398
alice <aliceng223@gmail.com>
2024-07-16 16:49:10 +0800

    add firework on header

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,10 +1,12 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Firework from "./fireworks/Firework";
 
 const Header = () => {
     return (
-        <header className="flex justify-between sticky top-0 w-full bg-white shadow-md p-4">
-            <h1 className="text-xl font-bold">My Sticky Header</h1>
-            <ConnectButton />
+        <header className="relative flex justify-between items-center sticky top-0 w-full bg-white shadow-md p-4 overflow-hidden">
+            <Firework />
+            <h1 className="text-xl font-bold z-10 text-white">CMU</h1>
+            <ConnectButton className="z-10" />
         </header>
     );
 };
