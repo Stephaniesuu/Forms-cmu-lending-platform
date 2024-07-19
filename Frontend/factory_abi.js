f93a85e05192a891bda6b2bb3e5b2190cfae4663
alan <alanhungyl@gmail.com>
2024-07-19 11:53:22 +0800

    re-arranged the ABIs into two .js file

---
+++ Frontend/factory_abi.js
@@ -0,0 +1,5 @@
+export const factory_ABI = [
+    "constructor()",
+    "function createLoanContract(address buyer, address seller, uint256 colleteralAmount, uint256 loanAmount, uint256 loanDuration, address collateralCoinAddress, address loanCoinAddress) external returns (address)",
+    "function getAddressWithIndex(uint256 index) external view returns (address)"
+  ];
