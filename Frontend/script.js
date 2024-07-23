a3e0d7ca988289adaa369be953dffee8f1481523
alan <alanhungyl@gmail.com>
2024-07-23 10:55:19 +0800

    first few code for script.js (with errors)

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,11 +1,53 @@
 import { ethers } from "ethers";
-import { contrat_abi } from "./contract_abi.js";
-import { factory_abi } from "./factory_abi.js";
+import 'dotenv/config';
+import  fs from "fs";
 
-const INFURA_ID = "13712e50d45a4432ad8e68aba775b198";
-const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`);
+const createContract = async (
+    contractFactory,
+    buyer,
+    seller,
+    collateralAmount,
+    loanAmount,
+    loanDuration,
+    collateralCoinAddress,
+    loanCoinAddress
+) => {
+    await contractFactory.createLoanContract(
+        buyer,
+        seller,
+        collateralAmount,
+        loanAmount,
+        loanDuration,
+        collateralCoinAddress,
+        loanCoinÍAddress
+    );
+};
 
+const getContractAddress = async(contractFactory, index) => {
+    contractFactory.getContractAddress(index);
+}
 
+const sellerRepay = async(contract, amount) => {
+    return await contract.sellerRepay(amount);
+}
 
+const contract_abi = JSON.parse(fs.readFileSync("./abi/LoanContract.json")).abi;
+const contract_bytecode = JSON.parse(fs.readFileSync("./abi/LoanContract.json")).bytecode;
 
+const main = async () => {
+    // read data from .env
+    const provider_url = process.env.provider_url;
+    const provider_ID = process.env.provider_ID;
+    const signer = process.env.signer;
 
+    // reading abi& bytecode from file
+    const factory_abi = JSON.parse(fs.readFileSync("./abi/LoanContractFactory.json")).abi;
+    const factory_bytecode = JSON.parse(fs.readFileSync("./abi/LoanContractFactory.json")).bytecode;
+
+    const provider = new ethers.JsonRpcProvider(`${provider_url}${provider_ID}`);
+
+    // create the factory
+    contractFactory = new ethers.ContractFactory(factory_abi, factory_bytecode, signer);
+
+}
+main()
