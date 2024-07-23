af5aa11ef7f758595841e4124d576f1965aac15c
Jorey717 <609019479@qq.com>
2024-07-23 14:34:26 +0800

    web3config

--- cmu-lending-platform/src/web3config.tsx
+++ cmu-lending-platform/src/web3config.tsx
@@ -7,22 +7,39 @@ import {
     arbitrum,
     base,
     sepolia,
+    mantle,
   } from 'wagmi/chains';
 
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { http } from '@wagmi/core';
 
-
+const mantleChain = {
+  id: 5003, // Mantle 网络的 Chain ID
+  name: 'Mantle',
+  network: 'mantle',
+  nativeCurrency: {
+    name: 'Mantle',
+    symbol: 'MNT',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: 'https://rpc.sepolia.mantle.xyz/',
+  },
+  blockExplorers: {
+    default: { name: 'Mantle Explorer', url: 'https://explorer.sepolia.mantle.xyz/' },
+  },
+  testnet: false,
+};
 export const config = getDefaultConfig({
     appName: 'cmu-lending-platform',
     projectId: '0f19e038366ede3e72b702d62f1eafbc',
-    chains: [sepolia, mainnet, polygon, optimism, arbitrum, base],
+    chains: [sepolia, mainnet, polygon, optimism, arbitrum, base, mantleChain],
     ssr: true,
     transports:{
       
         [mainnet.id]: http(),
         [sepolia.id]: http(),
-      
+        [mantleChain.id]: http(mantleChain.rpcUrls.default),
     }
   });
   
